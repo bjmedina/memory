071d87640fb2b5d7e9701cdaf9838e7af727044f
Bryan Medina <bjmedina@mit.edu>
2026-02-27 09:29:33 -0500

    something abotu this fitting procedure is not totaly working..... i mean it gets us in the ball park but not REALLY.... there is something about the subset performance that is easier to replicate than full performance...

--- slurm-scripts/run_sigma1_stability.sh
+++ slurm-scripts/run_sigma1_stability.sh
@@ -8,13 +8,22 @@
 #SBATCH --gres=gpu:1
 #SBATCH -o logs/sigma1_stability_%j.out
 #SBATCH -e logs/sigma1_stability_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bryanmaildina@gmail.com
 
 source activate /om2/user/gelbanna/miniconda3/envs/asr312
 cd /om2/user/bjmedina/auditory-memory/memory || exit 1
 
 NB_IN=notebooks/2026-02-19_sigma1-stability.ipynb
-NB_OUT=notebooks/2026-02-19_sigma1-stability_executed.ipynb
 
+# Timestamp for output naming
+TS="$(date +'%Y-%m-%d_%H%M%S')"
+
+# Make executed notebook name include timestamp
+BASE_IN="$(basename "$NB_IN" .ipynb)"
+NB_OUT="notebooks/executed/${BASE_IN}-${TS}.ipynb"
+
+echo "Will save to: $NB_OUT"
 echo "=== Running sigma1 stability notebook ==="
 echo "Start: $(date)"
 
